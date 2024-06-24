68234892f46a0fdc498c455fc3fffb3f64d81cbb
Ali Hassan <172230399+Ali-H7@users.noreply.github.com>
2024-06-24 18:35:17 +0300

    Complete Fifth Js exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
-
-};
-
+const sumAll = function(sum1, sum2) {
+    if (sum1 < 0 || sum2 < 0) return "ERROR"
+    if (!Number.isInteger(sum1) || !Number.isInteger(sum2)) return "ERROR"
+    let totalsum = 0;
+    let start = Math.min(sum1, sum2);
+    let end = Math.max(sum1, sum2);
+    for (let i = start; i <=end; i++ ) {
+        totalsum += i;
+    }
+    return totalsum;
+}
 // Do not edit below this line
 module.exports = sumAll;
+
+
+// take two sum numbers 
+// identify the numbers between them and add them into a variable
+// add all numbers together sum1 sum 2 and new var 
+// return 
