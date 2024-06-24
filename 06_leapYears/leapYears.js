147769d2649e2bfa9f40cfdcd542766941cbd593
Ali Hassan <172230399+Ali-H7@users.noreply.github.com>
2024-06-24 19:57:29 +0300

    Complete Sixth Js exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let divideBy4 = year / 4;
+    let divideBy100 = year / 100;
+    let divideBy400 = year / 400;
 
+    if (Number.isInteger(divideBy4) && !Number.isInteger(divideBy100) || Number.isInteger(divideBy400)) {
+        return true
+    } else {
+        return false
+    }
 };
 
 // Do not edit below this line
