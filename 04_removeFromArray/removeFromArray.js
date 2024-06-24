cb3ece23a915cad01c9e84aa0b438a1b8f8e98da
Ali Hassan <172230399+Ali-H7@users.noreply.github.com>
2024-06-24 16:02:04 +0300

    Complete Fourth Js exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array,...removeFromArray) {
+        const newArray = Array.from(removeFromArray);
+        const finalArray = array.filter(x=>!removeFromArray.includes(x));
+        return finalArray;
+    }
 
 // Do not edit below this line
 module.exports = removeFromArray;
